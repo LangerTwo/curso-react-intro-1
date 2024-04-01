060fe9277f761349520e0f605e7d4971ee36643e
LangerTwo <adrangel95@gmail.com>
2024-04-01 15:41:40 -0500

    TODO Counter actualizado con useContext

--- src/components/TodoCounter.jsx
+++ src/components/TodoCounter.jsx
@@ -1,9 +1,14 @@
-// import './TodoCounter.css'
+import { useContext } from "react"
+import { TodoContext } from "../context/TodoContext"
 
-const TodoCounter = ({ complete, total }) => {
+const TodoCounter = () => {
+    const { 
+      completeTodos,
+      totalTodos,
+    } = useContext(TodoContext)
     return (
         <h1 className="py-5 text-3xl font-bold text-indigo-400">
-          Has completado <span className="text-red-500"> {complete}</span> de <span className="text-green-500"> {total}</span> TODOS
+          Has completado <span className="text-red-500"> {completeTodos}</span> de <span className="text-green-500"> {totalTodos}</span> TODOS
         </h1>
     )
 }
